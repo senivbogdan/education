d4ddda50ac7cb78fb40aad308e389c349845fa31
senivbogdan <senivbogdan@github.com>
2021-12-01 11:00:26 +0300

    theme2Task2

--- 2. Data Types/2) sum-check/index.js
+++ 2. Data Types/2) sum-check/index.js
@@ -1,7 +1,14 @@
 function sumCheck(a, b) {
-  // Напиши свой код здесь
+  let num = 2
+  try {
+      if (typeof a === typeof num && typeof b === typeof num) {
+          return a + b
+      }
+      throw new Error("Wrong arguments type!")
+  } catch (error) {
+      throw new Error(error)
+  }
 }
-
 window.sumCheck = sumCheck;
 
 export default sumCheck;

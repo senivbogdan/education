81056d7242aaf4e800241e4213eecb3ecbd19574
senivbogdan <senivbogdan@github.com>
2021-12-02 13:19:36 +0300

    task2

--- 3. Array/2) find-pair/index.js
+++ 3. Array/2) find-pair/index.js
@@ -1,6 +1,11 @@
-function findPair(arr) {
-  // Напиши свой код здесь
-};
+function findPair(array) {
+    const arr = array
+    const sorted = arr.sort((a, b) => a - b)
+    for (let i = 0; i < sorted.length; i++) {
+        if (sorted[i] === sorted[i + 1]) return (sorted[i])
+    }
+    return null
+}
 
 window.findPair = findPair;
 

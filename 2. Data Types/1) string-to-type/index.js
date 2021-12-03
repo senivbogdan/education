3c2a2e98afbae77593560f9ba8772bb0e7c8db55
senivbogdan <senivbogdan@github.com>
2021-12-03 10:47:40 +0300

    tm2Task

--- 2. Data Types/1) string-to-type/index.js
+++ 2. Data Types/1) string-to-type/index.js
@@ -1,6 +1,23 @@
 function stringToType(str) {
-  // Напиши свой код здесь
-};
+  switch (str) {
+    case "null":
+      return null
+    case "undefined" :
+      return undefined
+    case "true" :
+      return true
+    case "false" :
+      return false
+    case "" :
+      return ""
+    default : {
+      return isNaN(str) ? str : Number(str)
+    }
+      break
+  }
+  // let kek = JSON.parse(str)
+  // if (typeof kek === "number") return kek
+}
 
 window.stringToType = stringToType;
 

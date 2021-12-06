c0c005443b1660e6658c8bd5d71e7828f780c1dd
senivbogdan <senivbogdan@github.com>
2021-12-06 15:48:32 +0300

    tm3Task4 rev1

--- 3. Array/4) query-object/index.js
+++ 3. Array/4) query-object/index.js
@@ -1,7 +1,25 @@
 function queryToObject(query) {
-  // Напиши свой код здесь
-};
-
+    const str = query.slice(1)
+    const arr = str.split("&").map(i => i.split("="))
+    const keys = []
+    const values = []
+    for (let i = 0; i < arr.length; i++) {
+        keys.push(arr[i][0])
+        values.push(arr[i][1])
+    }
+    for (let i = 0; i < values.length; i++) {
+        if (values[i] === "true") values[i] = true
+        else if (values[i] === "false") values[i] = false
+        else if (values[i] === "null") values[i] = null
+        else if (values[i] === "undefined") values[i] = undefined
+        else if (values[i] / 1 === Number(values[i])) values[i] = Number(values[i])
+    }
+    const obj = {}
+    for (let i = 0; i < keys.length; i++) {
+        obj[keys[i]] = values[i]
+    }
+    return obj
+}
 window.queryToObject = queryToObject;
 
 export default queryToObject;

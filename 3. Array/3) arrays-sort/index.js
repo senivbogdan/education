c2a5c687deac54af1c4500bc4ac3b8371148acfd
senivbogdan <senivbogdan@github.com>
2021-12-06 12:53:16 +0300

    tm3Task3 rev1

--- 3. Array/3) arrays-sort/index.js
+++ 3. Array/3) arrays-sort/index.js
@@ -1,7 +1,23 @@
 function arraysSort(arr) {
-  // Напиши свой код здесь
-};
+    function findMax(arr) {
+        let max = 0
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i]
+            }
+        }
+        return max
+    }
 
+    arr.sort((a, b) => {
+        if (findMax(a) > findMax(b)) {
+            return 1
+        } else if (findMax(a) < findMax(b)) {
+            return -1
+        } else return 0
+    })
+    return [...arr]
+}
 window.arraysSort = arraysSort;
 
 export default arraysSort;

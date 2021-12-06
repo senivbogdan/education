a436f12f356f876646b0ebbeac05048dcfa36cdc
senivbogdan <senivbogdan@github.com>
2021-12-06 10:08:47 +0300

    tm4Task1 rev

--- 4. Async/1) async-sum/index.js
+++ 4. Async/1) async-sum/index.js
@@ -1,8 +1,8 @@
-function asyncSum(a, b) {
+function asyncSum(a, b){
     return new Promise(((resolve, reject) => {
         if (typeof a !== "number" || typeof b !== 'number'){
             reject(new Error())
-        } else setTimeout(()=> resolve(a + b),1000)
+        } else setTimeout(() => resolve(a + b),1000)
     }))
 }
 

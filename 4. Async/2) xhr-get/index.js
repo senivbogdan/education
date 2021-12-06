3cb08818049b0ca377839a53e959e0c3152679c4
senivbogdan <senivbogdan@github.com>
2021-12-06 12:28:22 +0300

    tm4Task2 rev2

--- 4. Async/2) xhr-get/index.js
+++ 4. Async/2) xhr-get/index.js
@@ -1,10 +1,10 @@
 function xhrGet(url) {
-    const xhr = new XMLHttpRequest()
-    xhr.open("GET", url, false)
-    xhr.setRequestHeader("Content-Type", "application/json")
-    xhr.responseType = "json"
-    xhr.send()
     return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest()
+        xhr.open("GET", url, false)
+        xhr.setRequestHeader("Content-Type", "application/json")
+        xhr.responseType = "json"
+        xhr.send()
         if (xhr.status >= 400) {
             reject(xhr.response)
         } else resolve(xhr.response)

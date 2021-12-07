b1dcc800745ef1e1878e66b95057820e61285ad7
senivbogdan <senivbogdan@github.com>
2021-12-07 14:01:51 +0300

    tm3Task2 find

--- 3. Array/2) find-pair/index.js
+++ 3. Array/2) find-pair/index.js
@@ -1,10 +1,5 @@
 function findPair(array) {
-    const arr = array
-    const sorted = arr.sort((a, b) => a - b)
-    for (let i = 0; i < sorted.length; i++) {
-        if (sorted[i] === sorted[i + 1]) return (sorted[i])
-    }
-    return null
+    return  [...array].sort((a, b) => a - b).find((item, index,array)=> item === array[index+1]) || null;
 }
 
 window.findPair = findPair;

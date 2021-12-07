77ed010387d6db8001315db2c9c9881b6b6ec26d
senivbogdan <senivbogdan@github.com>
2021-12-07 13:59:12 +0300

    tm3Task1 reduce

--- 3. Array/1) full-sum/index.js
+++ 3. Array/1) full-sum/index.js
@@ -1,27 +1,11 @@
 function fullSum(...args) {
     try {
-        console.log(args)
         if (args.length === 0) return 0
-        if (args.includes(undefined) || args.includes(false) ||
-            args.includes([]) || args.includes("1")) {
-            throw new Error("Wrong Argument Type")
-        }
-        let flag = 0
-        for (let i = 0; i < args.length; i++) {
-            if (args[i].length === 0) {
-                flag = flag + 1
-            }
-        }
-        if (flag != 0) {
-            throw new Error("Wrong Argument Type")
-        }
-        let array = args
-        let sum = 0
-        array.forEach((item) => {
-            sum += item
+        let sum = args.reduce((prev,item)=>{
+            return prev + item
         })
-        // if (typeof arr != "number") throw new Error("Wrong Argument Type")
-        return sum
+        if (args.every(item => typeof item === "number" )) return sum
+        else throw new Error(e)
     } catch (e) {
         throw new Error(e)
     }
